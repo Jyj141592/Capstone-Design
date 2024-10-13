597e7d5352a29260f8a1286e6246b2a5c89373bd
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-10-13 20:01:55 +0900

    character, camera

--- Phyzzle/Assets/Scripts/InGame/Camera/CameraFollow.cs
+++ Phyzzle/Assets/Scripts/InGame/Camera/CameraFollow.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform follow;
+    public CinemachineVirtualCamera cam;
     public float depth = 10.0f;
-    public Vector2 offset = Vector2.zero;
+    public Vector3 offset = Vector2.zero;
+    public float topRadius = 10.0f;
+    public float middleRadius = 10.0f;
+    public float bottomRadius = 1.0f;
     public float sensitive = 1.0f;
+    private float topAngle = 89.0f;
+    private float middleAngle = 0.0f;
+    private float bottomAngle = -50.0f;
     private PlayerController controller;
-    private float angle = 0.0f;
+    private float angleY = 0.0f;
+    private float angleX = 0.0f;
     void Start()
     {
         controller = follow.GetComponent<PlayerController>();
@@ -27,17 +36,37 @@ public class CameraFollow : MonoBehaviour
         }
     }
     void UpdatePosition(){
-        Vector3 pos = new Vector3(-Mathf.Sin(angle), 0, -Mathf.Cos(angle));
-        transform.forward = -pos;
-        pos *= depth;
-        pos += transform.up * offset.y + transform.right * offset.x + follow.position;
-        transform.position = pos;
-        controller.SetCameraPosition(transform.position);
+        Vector3 pos = new Vector3(-Mathf.Sin(angleY), 0, -Mathf.Cos(angleY));
+        transform.rotation = Quaternion.Euler(0, angleY, 0);
+        // transform.forward = -pos;
+        // transform.up = Vector3.down;
+
+        // pos *= depth;
+        // pos += transform.up * offset.y + transform.right * offset.x + follow.position;
+        // transform.position = pos;
+        transform.position = transform.up * offset.y + transform.right * offset.x + pos * offset.z + follow.position;
+        
+        float radius;
+        if(angleX > middleAngle){
+            radius = topRadius * ((angleX - middleAngle) / (topAngle - middleAngle)) +
+                    middleRadius * ((topAngle - angleX) / (topAngle - middleAngle));
+        }
+        else{
+            radius = middleRadius * ((angleX - bottomAngle) / (middleAngle - bottomAngle)) +
+                    bottomRadius * ((middleAngle - angleX) / (middleAngle - bottomAngle));
+        }   
+        Vector3 p = new Vector3(0, radius * Mathf.Sin(angleX * Mathf.Deg2Rad), -radius * Mathf.Cos(angleX * Mathf.Deg2Rad));
+        cam.transform.localPosition = p;
+        cam.transform.localRotation = Quaternion.Euler(angleX, 0, 0);
+        //cam.transform.forward = transform.position - cam.transform.position;
+        controller.SetCameraPosition(cam.transform.position, cam.transform.forward);
     }
     void OnViewRotate(InputAction.CallbackContext context){
         if(context.performed){
             Vector2 value = context.ReadValue<Vector2>();
-            angle += value.x * 0.1f * sensitive;
+            angleY += value.x * 0.3f * sensitive;
+            angleX -= value.y * 0.3f * sensitive;
+            angleX = Mathf.Clamp(angleX, bottomAngle, topAngle);
         }
     }
 }
--- Phyzzle/Assets/Scripts/InGame/Player/PlayerController.cs
+++ Phyzzle/Assets/Scripts/InGame/Player/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rigid;
     
     // Status
+    public bool isMovable{
+        get => animator.GetCurrentAnimatorStateInfo(0).tagHash != haltHash;
+    }
     private Vector2 move = Vector2.zero;
     private float maxSpeed = 10.0f;
     private bool isAir = false;
@@ -18,17 +21,23 @@ public class PlayerController : MonoBehaviour
         get; private set;
     } = false;
     private Vector3 moveDir = Vector3.zero;
+    private bool isHold = false;
 
     // Hashs
+    private int haltHash;
     private int moveHash;
     private int isAirHash;
     private int jumpHash;
+    private int holdHash;
+    private int throwHash;
 
     // Camera
     private Vector3 cameraPos = Vector3.zero;
+    private Vector3 cameraLook = Vector3.forward;
 
     // Objects
-    public GameObject hand;
+    public GameObject handPos;
+    public Transform hand;
     private Collider hold = null;
     void Start()
     {
@@ -37,19 +46,23 @@ public class PlayerController : MonoBehaviour
         InputManager.instance.RegisterCallback(InputMap.InGame,"Horizontal",OnHorizontal);
         InputManager.instance.RegisterCallback(InputMap.InGame,"Jump",OnJump);
         InputManager.instance.RegisterCallback(InputMap.InGame, "Hold", OnHold);
+        InputManager.instance.RegisterCallback(InputMap.InGame, "Throw", OnThrow);
 
         // Components
         animator = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
 
         // Hashs
+        haltHash = Animator.StringToHash("halt");
         moveHash = Animator.StringToHash("move");
         isAirHash = Animator.StringToHash("isAir");
         jumpHash = Animator.StringToHash("jump");
+        holdHash = Animator.StringToHash("hold");
+        throwHash = Animator.StringToHash("throw");
     }
 
     private void Update() {
-        if(move.x != 0 || move.y != 0){
+        if(isMovable && (move.x != 0 || move.y != 0)){
             
             Vector3 forward = transform.position - cameraPos;
             forward.y = 0;
@@ -94,11 +107,33 @@ public class PlayerController : MonoBehaviour
             InputManager.instance.RemoveCallback(InputMap.InGame,"Horizontal",OnHorizontal);
             InputManager.instance.RemoveCallback(InputMap.InGame,"Jump",OnJump);
             InputManager.instance.RemoveCallback(InputMap.InGame, "Hold", OnHold);
+            InputManager.instance.RemoveCallback(InputMap.InGame, "Throw", OnThrow);
         }
     }
 
-    public void SetCameraPosition(Vector3 cameraPos){
+    public void SetHold(){
+        hold.attachedRigidbody.isKinematic = true;
+        hold.gameObject.transform.SetParent(hand);
+        hold.gameObject.transform.localPosition = Vector3.zero;
+        hold.gameObject.transform.localRotation = Quaternion.identity;
+        hold.enabled = false;
+    }
+    public void ClearHold(){
+        hold.enabled = true;
+        hold.gameObject.transform.SetParent(null);
+        hold.attachedRigidbody.isKinematic = false;
+        hold = null;
+    }
+    public void Throw(){
+        hold.enabled = true;
+        hold.gameObject.transform.SetParent(null);
+        hold.attachedRigidbody.isKinematic = false;
+        hold.attachedRigidbody.AddForce((transform.forward + new Vector3(0,0.9f,0)) * 800);
+    }
+
+    public void SetCameraPosition(Vector3 cameraPos, Vector3 cameraLook){
         this.cameraPos = cameraPos;
+        this.cameraLook = cameraLook;
     }
 
     public void OnVertical(InputAction.CallbackContext context){
@@ -140,22 +175,37 @@ public class PlayerController : MonoBehaviour
         }
     }
     public void OnHold(InputAction.CallbackContext context){
-        if(context.started){
-            if(hold != null){
-                hold.gameObject.transform.SetParent(null);
-                hold.attachedRigidbody.isKinematic = false;
-                hold = null;
+        if(isMovable && context.started){
+            //animator.SetBool(holdHash, !animator.GetBool(holdHash));
+            if(isHold){
+                // hold.gameObject.transform.SetParent(null);
+                // hold.attachedRigidbody.isKinematic = false;
+                // hold = null;
+                isHold = false;
+                animator.SetBool(holdHash, false);
             }
             else{
-                var colls = Physics.OverlapSphere(hand.transform.position, 0.7f, LayerMask.GetMask("Cube"));
+                var colls = Physics.OverlapSphere(handPos.transform.position, 0.7f, LayerMask.GetMask("Cube"));
                 if(colls.Length > 0){
-                    colls[0].attachedRigidbody.isKinematic = true;
-                    colls[0].gameObject.transform.SetParent(hand.transform);
-                    colls[0].gameObject.transform.localPosition = Vector3.zero;
+                    isHold = true;
+                    animator.SetBool(holdHash, true);
                     hold = colls[0];
+                    // colls[0].attachedRigidbody.isKinematic = true;
+                    // colls[0].gameObject.transform.SetParent(hand.transform);
+                    // colls[0].gameObject.transform.localPosition = Vector3.zero;
                 }
             }
             
         }
+    }
+    public void OnThrow(InputAction.CallbackContext context){
+        if(isMovable && isHold && context.started){
+            isHold = false;
+            animator.SetTrigger(throwHash);
+            animator.SetBool(holdHash, false);
+        }
+    }
+    public void OnPush(InputAction.CallbackContext context){
+        
     }
 }
